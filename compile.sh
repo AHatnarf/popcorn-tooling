31c15d78fc86ccf8226cb353459e006bd042333e
Andrew Hughes <ah@narfindustries.com>
2020-07-14 06:08:46 -0400

    Update for newer docker image

--- compile.sh
+++ compile.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Check to see if we have node count
+if ! [[ -z "$NUMNODES" ]]
+then
+  echo "Found x86 node count"
+else
+  NUMNODES=2
+fi
+
+echo "Number of x86 nodes: $NUMNODES"
+NUMNODES="$(($NUMNODES-1))"
+
 # Check for kvm support
 KVMFLAGS='-smp 2 -m 1024 -no-reboot -nographic -kernel /app/linux/arch/x86/boot/bzImage'
 if [ $(ls /dev | grep kvm | wc -l) -eq 1 ]; then
@@ -10,34 +21,44 @@ fi
 
 # Run build tests
 echo "Compiling the kernel"
-cd linux #&& make clean -j$(nproc) &> /dev/null
+cd linux && make clean -j$(nproc) &> /dev/null
 make CCACHE_DIR=/app/ccache CC="ccache gcc" -j$(nproc) &> /app/logs/kernel_compiliation
 EXITCODE=$?
 test $EXITCODE -eq 0 && echo "Kernel compiliation successful!" || exit -1;
 
 # Build msg_layer drivers
 echo "Building drivers/msg_layer drivers"
-cd drivers/msg_layer && ls &&  make CCACHE_DIR=/app/ccache CC="ccache gcc" -j$(nproc) &> /app/logs/msg_compiliation
+cd drivers/msg_layer && make CCACHE_DIR=/app/ccache CC="ccache gcc" -j$(nproc) &> /app/logs/msg_compiliation
 EXITCODE=$?
 test $EXITCODE -eq 0 && echo "msg_layer compiliation successful!" || exit -2;
 
+# Creating disks
+cd /app/disks
+for ((i=0; i <= $NUMNODES; i++)); do
+  qemu-img create -f qcow2 -o backing_file=base.img ${i}.img > /dev/null
+done
+
+# Create nodes list
+echo "" > /app/nodes
+for ((i=0; i <= $NUMNODES; i++)); do
+  echo "10.4.4.$(($i+100))" >> /app/nodes
+done
+
 # Start Virtual Machines & Networking
 echo "Starting virtual machines"
-brctl addbr br0
-ifconfig br0 10.4.4.1 netmask 255.255.255.0
+sudo brctl addbr br0
+sudo ifconfig br0 10.4.4.1 netmask 255.255.255.0
 
 cd /app
-for i in {0..3}
-do
-  echo 'qemu-system-x86_64 '${KVMFLAGS}' -gdb tcp::123'${i}' -drive id=root,media=disk,file=/app/disks/'${i}'.img -net nic,macaddr=00:da:bc:de:00:1'$((3+${i}))' -net tap,ifname=tap'${i}' -append "root=/dev/sda1 console=ttyS0 ip=10.4.4.'$((100+${i}))'" 2>&1 | tee /app/logs/vm'${i} > launchVM${i}.sh
+for ((i=0; i <= $NUMNODES; i++)); do
+  echo 'sudo qemu-system-x86_64 '${KVMFLAGS}' -gdb tcp::123'${i}' -drive id=root,media=disk,file=/app/disks/'${i}'.img -net nic,macaddr=00:da:bc:de:00:1'$((3+${i}))' -net tap,ifname=tap'${i}' -append "root=/dev/sda1 console=ttyS0 ip=10.4.4.'$((100+${i}))'" 2>&1 | tee /app/logs/vm'${i} > launchVM${i}.sh
   chmod +x launchVM${i}.sh && screen -S ${i} -d -m ./launchVM${i}.sh
 done
 
 sleep 2
 
-for i in {0..3}
-do
-  brctl addif br0 tap${i}
+for ((i=0; i <= $NUMNODES; i++)); do
+  sudo brctl addif br0 tap${i}
   EXITCODE=$?
   test $EXITCODE -eq 0 && echo "Node "${i}" started!" || exit 10;
 done
@@ -50,8 +71,7 @@ EXITCODE=$?
 test $EXITCODE -eq 0 && echo "Popcorn-lib compiliation successful!" || exit -3;
 
 # Wait for both Virtual Machines to start
-for i in {0..3}
-do
+for ((i=0; i <= $NUMNODES; i++)); do
   while ! (exec 3<>/dev/tcp/10.4.4.$((100+${i}))/22) &> /dev/null
   do
     echo "Waiting for node "${i}" to come online" && sleep 2
@@ -61,10 +81,10 @@ done
 sleep 2
 
 # Insert modules and run popcorn tests
-for i in {0..3}
-do
+for ((i=0; i <= $NUMNODES; i++)); do
   sshpass -p "popcorn" scp -o StrictHostKeyChecking=no /app/linux/drivers/msg_layer/msg_socket.ko popcorn@10.4.4.$((100+${i})):/home/popcorn
   sshpass -p "popcorn" scp -o StrictHostKeyChecking=no -r /app/popcorn-lib popcorn@10.4.4.$((100+${i})):/home/popcorn
+  sshpass -p "popcorn" scp -o StrictHostKeyChecking=no -r /app/nodes popcorn@10.4.4.$((100+${i})):/etc/popcorn/nodes
   sshpass -p "popcorn" ssh -o StrictHostKeyChecking=no  popcorn@10.4.4.$((100+${i})) "sudo insmod msg_socket.ko" &
 done
 
@@ -75,6 +95,7 @@ declare -a tests=(
   "./ft2c 0 1"
   )
 
+sleep 20
 echo -e "Running tests\n=============="
 
 # A bit of a quick hacky way to run tests
@@ -86,4 +107,11 @@ do
 done
 
 # Wait at the end for interactive testing
-tail -f /dev/null
+if ! [[ -z "$INTERACTIVE" ]]
+then
+  echo "Interactive variable set, pausing"
+  echo "Check docs on how to interact with the container"
+  tail -f /dev/null
+else
+  echo "Ending tests, set INTERACTIVE to debug/interact with containers"
+fi
